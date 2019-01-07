4c5a9959f3fc925dbb078ebf3ed0d3d794a03155
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2019-01-07 15:45:45 -0600

    updated controller, index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,9 +5,38 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var sandwich = { burgers: data };
+    console.log(sandwich);
+    res.render("index", sandwich);
+  });
+});
 
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      res.json({ id: result.insertId });
+    });
+});
 
+router.put("/api/burgers/:id", function (req, res) {
 
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+
+  burger.updateOne(
+    { devoured: req.body.devoured },
+    condition, function (result) {
+      if (result.changedRows == 0) {
+        return res.status(500).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
 
 
 
