ac8f03bce75cd4a43d05b8e6d46eea641238539f
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2019-01-07 23:24:26 -0600

    update, staging for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+connection = mysql.createConnection({
 
     host: "localhost",
     port: 3306,
@@ -8,6 +14,7 @@ var connection = mysql.createConnection({
     password: "Vikings-Win",
     database: "burgers_db"
 });
+};
 
 connection.connect(function (err) {
     if (err) {
